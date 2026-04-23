627d85b6c104923f07061b8f9cdc283a5c3b95fb
Rodrigo Costa <Rodrigocostas147@gmail.com>
2026-04-23 10:59:17 -0300

    Add files via upload

---
+++ Logistics & Enterprise Management API/AtividadePontuada/src/main/java/com/example/AtividadePontuada/controller/Welcome.java
@@ -0,0 +1,25 @@
+package com.example.AtividadePontuada.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+@RestController
+public class Welcome {
+    @GetMapping("/")
+    public Map<String, String> welcome() {
+        return Map.of(
+                "mensagem", "Bem-vindo à API da Atividade Pontuada!");
+
+
+    }
+
+   
+    @GetMapping("/dev")
+    public Map<String, String> devInfo() {
+        return Map.of(
+                "desenvolvedor", "Rodrigo Costa"
+        );
+    }
+}
+
---
+++ Logistics & Enterprise Management API/AtividadePontuada/src/main/java/com/example/AtividadePontuada/model/FuncionariosModel.java
@@ -0,0 +1,105 @@
+package com.example.AtividadePontuada.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class FuncionariosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String matricula;
+    private String dt_nascimento;
+    private String setor;
+    private double salario;
+    private String telefone;
+    private String email;
+    private String endereco;
+
+    public FuncionariosModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getDt_nascimento() {
+        return dt_nascimento;
+    }
+
+    public void setDt_nascimento(String dt_nascimento) {
+        this.dt_nascimento = dt_nascimento;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
